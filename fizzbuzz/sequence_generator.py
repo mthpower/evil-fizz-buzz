d65153e6c0ed79621cb3751b4d425c9f17026b97
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2017-05-12 13:33:18 +0100

    added a sequence generator

---
+++ fizzbuzz/sequence_generator.py
@@ -0,0 +1,2 @@
+def sequence_generator(first, last):
+    return range(first, last+1)
--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -1,2 +1,9 @@
-def test_an_empty_repo():
-    assert True
+import unittest
+
+from fizzbuzz.sequence_generator import sequence_generator
+
+
+class TestEvilFizzBuzz(unittest.TestCase):
+    def test_generating_a_sequence(self):
+        self.assertEqual(range(1, 101), sequence_generator(1, 100))
+    
